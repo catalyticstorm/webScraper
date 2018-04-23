d267d540bffc7e5832cca9189f068f8d6ef03dc2
catalyticstorm <32317951+catalyticstorm@users.noreply.github.com>
2018-04-23 18:58:04 -0400

    Added stuffs

---
+++ assets/scripts/commentator.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+var express = require("express");
+var mongojs = require("mongojs");
+var request = require("request");
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+
+var app = express();
